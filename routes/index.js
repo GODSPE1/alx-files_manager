4c60c817a32acb139843819215708cda201f624b
GODSPE1 <godspy18@gmail.com>
2024-09-05 00:30:55 +0100

    added more routes

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/stats', AppController.getStats);
 router.put('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users', UseController.getMe)
+router.get('/users/me', UseController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
 
 export default router;
